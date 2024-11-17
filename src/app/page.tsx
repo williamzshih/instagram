0b2fcee82c2b05c50bb7e86375a7f440853d89e4
William Shih <148172235+williamzshih@users.noreply.github.com>
2024-11-17 11:40:53 -0800

    Add sign in button authentification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
-export default function Home() {
+import { auth, signIn, signOut } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
   return (
-    <div className="bg-ig-red">test</div>
+    <div className="">
+      test
+      {session ? (
+        <form
+          action={async () => {
+            "use server"
+            await signOut();
+          }}
+        >
+          <button
+            className="border px-4 py-2 text-white bg-ig-red rounded-lg"
+            type="submit">Logout
+          </button>
+        </form>
+      ) : (
+        <form
+          action={async () => {
+            "use server"
+            await signIn("google")
+          }}
+        >
+          <button
+            className="border px-4 py-2 text-white bg-ig-red rounded-lg"
+            type="submit">Signin with Google
+          </button>
+        </form>
+      )}
+    </div>
   );
 }
