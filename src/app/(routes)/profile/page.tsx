ef3427b63d0c8065299c79aac6f88081c48e282c
William Shih <148172235+williamzshih@users.noreply.github.com>
2024-11-17 12:13:50 -0800

    Add navbar

--- src/app/(routes)/profile/page.tsx
+++ src/app/(routes)/profile/page.tsx
@@ -1,7 +1,22 @@
+import { BadgeCheck, ChevronLeft, Settings } from "lucide-react";
+
 export default function ProfilePage() {
     return (
         <div>
-            
+            <div className="flex justify-between items-center">
+                <button>
+                    <ChevronLeft />
+                </button>
+                <div className="font-bold flex items-center gap-2">
+                    user name
+                    <div className="size-5 rounded-full inline-flex justify-center items-center text-black">
+                        <BadgeCheck />
+                    </div>
+                </div>
+                <button>
+                    <Settings />
+                </button>
+            </div>
         </div>
     )
 }
