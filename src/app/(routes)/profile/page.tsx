dad3c85cf19283dd76aa83a62f0a4451e01beb33
William Shih <148172235+williamzshih@users.noreply.github.com>
2024-12-15 11:48:39 -0800

    Add more profile info and implement profile tabs

--- src/app/(routes)/profile/page.tsx
+++ src/app/(routes)/profile/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { ChevronLeft, Settings, BadgeCheck } from "lucide-react";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { useState } from "react";
 
 export default function Profile() {
+  const [selectedTab, setSelectedTab] = useState("posts");
+
   return (
     <div className="flex flex-col items-center justify-center p-4">
       <div className="flex items-center justify-between w-full mb-4">
@@ -17,12 +22,30 @@ export default function Profile() {
           <Settings />
         </Button>
       </div>
-      <div className="w-46 h-46 p-2 rounded-full flex items-center justify-center bg-gradient-to-tr from-ig-orange to-ig-red">
+      <div className="w-46 h-46 p-2 rounded-full flex items-center justify-center bg-gradient-to-tr from-ig-orange to-ig-red mb-4">
         <div className="bg-white w-42 h-42 p-2 rounded-full">
           <Avatar className="w-40 h-40">
             <AvatarImage src="https://picsum.photos/200/300" />
           </Avatar>
         </div>
+      </div>
+      <p className="text-xl font-bold">Name</p>
+      <p className="text-lg mb-4">Bio</p>
+      <div className="flex items-center justify-center gap-2">
+        <Button
+          variant={selectedTab === "posts" ? "default" : "ghost"}
+          className="text-lg"
+          onClick={() => setSelectedTab("posts")}
+        >
+          Posts
+        </Button>
+        <Button
+          variant={selectedTab === "highlights" ? "default" : "ghost"}
+          className="text-lg"
+          onClick={() => setSelectedTab("highlights")}
+        >
+          Highlights
+        </Button>
       </div>
     </div>
   );
