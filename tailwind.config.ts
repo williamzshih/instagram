4bed6bd0bca48e6bdf0c268bbf4b1c46395c97ff
William Shih <148172235+williamzshih@users.noreply.github.com>
2024-12-15 10:18:45 -0800

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "ig-red": "var(--red)",
+        "ig-orange": "var(--orange)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
