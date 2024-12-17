7f4418462f5eff6fc7c8dc350c7c8ff006205e3e
William Shih <148172235+williamzshih@users.noreply.github.com>
2024-12-17 14:51:32 -0800

    Add a mobile navigation bar

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import MobileNav from "@/components/MobileNav";
 
 const geistSans = localFont({
   src: "../fonts/GeistVF.woff",
@@ -30,7 +31,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster />
+        <MobileNav />
+        <Toaster closeButton richColors />
       </body>
     </html>
   );
---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { House, Search, Plus, LayoutGrid, User } from "lucide-react";
+
+export default function MobileNav() {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 flex items-center justify-center">
+      <div className="w-full p-2 flex items-center justify-evenly rounded-t-lg bg-white">
+        <Button variant="ghost" size="icon">
+          <Link href="/">
+            <House />
+          </Link>
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Link href="/search">
+            <Search />
+          </Link>
+        </Button>
+      </div>
+      <div className="flex items-center justify-center relative w-[183px]">
+        <div className="flex items-center justify-center p-2 border-[56px] rounded-full border-white border-t-transparent border-l-transparent rotate-45 absolute -top-[116px]">
+          <div className="border-4 border-transparent -rotate-45">
+            <Link
+              href="/create"
+              className="bg-gradient-to-tr from-ig-orange to-ig-red rounded-full text-white size-14 flex items-center justify-center"
+            >
+              <Plus />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="w-full p-2 flex items-center justify-evenly rounded-t-lg bg-white">
+        <Button variant="ghost" size="icon">
+          <Link href="/browse">
+            <LayoutGrid />
+          </Link>
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Link href="/profile">
+            <User />
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
