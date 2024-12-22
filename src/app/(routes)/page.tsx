11c223cfcc43a3de6db30227850d2b02fa920eef
William Shih <148172235+williamzshih@users.noreply.github.com>
2024-12-22 00:26:32 -0800

    Change start page

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,73 +1,59 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
-import { getUser } from "@/utils/actions";
+import { Button } from "@/components/ui/button";
+import { signInAction, signOutAction, getSession } from "@/utils/actions";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
-import { Plus } from "lucide-react";
-import GradientAvatar from "@/components/GradientAvatar";
-import Post from "./post/[id]/page";
 import { SyncLoader } from "react-spinners";
 
-export default function HomePage() {
+export default function SignInPage() {
+  const queryClient = useQueryClient();
+
   const {
-    data: user,
-    isPending: isUserPending,
-    error: userError,
+    data: session,
+    isPending: isSessionPending,
+    error: sessionError,
   } = useQuery({
-    queryKey: ["user", "homePage"],
-    queryFn: () => getUser(),
+    queryKey: ["session", "signInPage"],
+    queryFn: () => getSession(),
   });
 
-  if (isUserPending) {
+  const { mutate: signIn } = useMutation({
+    mutationFn: () => signInAction(),
+  });
+
+  const { mutate: signOut } = useMutation({
+    mutationFn: () => signOutAction(),
+    onSuccess: () => {
+      queryClient.clear();
+    },
+  });
+
+  if (isSessionPending)
     return (
-      <div className="flex flex-col items-center justify-center p-4">
+      <div className="flex flex-col justify-center items-center p-4">
         <SyncLoader />
       </div>
     );
-  }
 
-  if (userError) {
-    console.error(userError);
-    toast.error(userError as unknown as string);
+  if (sessionError) {
+    console.error(sessionError);
+    toast.error(sessionError as unknown as string);
 
     return (
-      <div className="flex flex-col items-center justify-center p-4 text-red-500">
-        {userError as unknown as string}
-      </div>
-    );
-  }
-
-  if (!user) {
-    return (
-      <div className="flex flex-col items-center justify-center p-4 text-red-500">
-        User not found
+      <div className="flex flex-col justify-center items-center p-4 text-red-500">
+        {sessionError as unknown as string}
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col items-center justify-center p-4 gap-4">
-      <div className="flex gap-4 self-start">
-        <div className="w-24 h-24 rounded-full border-2 flex items-center justify-center">
-          <Plus size={24} />
-        </div>
-        {user.following.map((user) => (
-          <div
-            key={user.whoTheyreFollowing.id}
-            className="flex flex-col items-center justify-center gap-1"
-          >
-            <GradientAvatar user={user.whoTheyreFollowing} size={20} />
-            <p className="text-sm text-gray-500">
-              {user.whoTheyreFollowing.username}
-            </p>
-          </div>
-        ))}
-      </div>
-      {user.following
-        .flatMap((user) => user.whoTheyreFollowing.posts)
-        .map((post) => (
-          <Post key={post.id} params={{ id: post.id }} fromHome />
-        ))}
+    <div className="flex flex-col justify-center items-center p-4">
+      {session ? (
+        <Button onClick={() => signOut()}>Sign out</Button>
+      ) : (
+        <Button onClick={() => signIn()}>Sign in</Button>
+      )}
     </div>
   );
 }
