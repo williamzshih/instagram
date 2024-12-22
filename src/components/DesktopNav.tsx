e2e72be0a89c82346e08f4502b813b279c2dc215
William Shih <148172235+williamzshih@users.noreply.github.com>
2024-12-21 23:46:43 -0800

    Refactor DesktopNav

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -7,8 +7,8 @@ import Link from "next/link";
 
 export default function DesktopNav() {
   return (
-    <div className="hidden md:flex flex-col gap-4 p-4 w-64 h-screen sticky top-0 shadow-md shadow-gray-500">
-      <Button variant="ghost" className="w-fit p-2">
+    <div className="hidden md:flex flex-col gap-4 p-4 h-screen sticky top-0 shadow-md shadow-gray-500">
+      <Button variant="ghost" className="w-fit h-fit">
         <Link href="/">
           <div className="flex items-center gap-2">
             <Image src={igLogo} alt="Instagram logo" width={30} height={30} />
@@ -22,43 +22,43 @@ export default function DesktopNav() {
           </div>
         </Link>
       </Button>
-      <Button variant="ghost" className="w-fit p-2">
+      <Button variant="ghost" className="w-fit h-fit">
         <Link href="/">
           <div className="flex items-center gap-4">
             <House size={30} absoluteStrokeWidth />
-            <p className="text-[16px]">Home</p>
+            <p className="text-lg">Home</p>
           </div>
         </Link>
       </Button>
-      <Button variant="ghost" className="w-fit p-2">
+      <Button variant="ghost" className="w-fit h-fit">
         <Link href="/search">
           <div className="flex items-center gap-4">
             <Search size={30} absoluteStrokeWidth />
-            <p className="text-[16px]">Search</p>
+            <p className="text-lg">Search</p>
           </div>
         </Link>
       </Button>
-      <Button variant="ghost" className="w-fit p-2">
+      <Button variant="ghost" className="w-fit h-fit">
         <Link href="/create">
           <div className="flex items-center gap-4">
             <Plus size={30} absoluteStrokeWidth />
-            <p className="text-[16px]">Create</p>
+            <p className="text-lg">Create</p>
           </div>
         </Link>
       </Button>
-      <Button variant="ghost" className="w-fit p-2">
+      <Button variant="ghost" className="w-fit h-fit">
         <Link href="/browse">
           <div className="flex items-center gap-4">
             <LayoutGrid size={30} absoluteStrokeWidth />
-            <p className="text-[16px]">Browse</p>
+            <p className="text-lg">Browse</p>
           </div>
         </Link>
       </Button>
-      <Button variant="ghost" className="w-fit p-2">
+      <Button variant="ghost" className="w-fit h-fit">
         <Link href="/profile">
           <div className="flex items-center gap-4">
             <User size={30} absoluteStrokeWidth />
-            <p className="text-[16px]">Profile</p>
+            <p className="text-lg">Profile</p>
           </div>
         </Link>
       </Button>
