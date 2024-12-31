b57cdbb94c924fc6c83fd1edf654695ccc8173f7
William Shih <148172235+williamzshih@users.noreply.github.com>
2024-12-31 10:21:28 -0800

    Update next config to include picsum photos

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "upload.wikimedia.org",
       },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
     ],
   },
 };
