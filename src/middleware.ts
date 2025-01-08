dd5b5e673a3cff0cff0ddfec5d63f7df3f0f9cff
William Shih <148172235+williamzshih@users.noreply.github.com>
2025-01-07 19:01:12 -0800

    Add middleware matcher for faster routing

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,23 +2,25 @@ import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
 export default auth(async (req) => {
-  if (req.nextUrl.pathname.startsWith("/user/")) {
-    const cookie = req.headers.get("cookie");
-    if (!cookie) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
-    const response = await fetch(`${req.nextUrl.origin}/api/user`, {
-      headers: {
-        cookie: cookie,
-      },
-    });
-    const username = await response.json();
-    const pathSplit = req.nextUrl.pathname.split("/user/")[1].split("/");
-    const pathUsername = pathSplit[0];
-    const rest = pathSplit.slice(1).join("/");
-    if (username === pathUsername) {
-      return NextResponse.redirect(new URL(`/profile/${rest}`, req.nextUrl));
-    }
-    return NextResponse.next();
+  const cookie = req.headers.get("cookie");
+  if (!cookie) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
+  const response = await fetch(`${req.nextUrl.origin}/api/user`, {
+    headers: {
+      cookie: cookie,
+    },
+  });
+  const username = await response.json();
+  const pathSplit = req.nextUrl.pathname.split("/user/")[1].split("/");
+  const pathUsername = pathSplit[0];
+  const rest = pathSplit.slice(1).join("/");
+  if (username === pathUsername) {
+    return NextResponse.redirect(new URL(`/profile/${rest}`, req.nextUrl));
+  }
+  return NextResponse.next();
 });
+
+export const config = {
+  matcher: "/user/:path*",
+};
