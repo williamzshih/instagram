2ec6a2bc55cc9f85471ead9c7da81e41ded494f7
William Shih <148172235+williamzshih@users.noreply.github.com>
2025-01-20 03:04:50 -0800

    Fix instagram text size

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -42,7 +42,7 @@ export default function DesktopNav() {
               }
               alt="Instagram text"
               width={100}
-              height={100}
+              height={36}
               className="mt-1"
             />
           </div>
