5afff5f32206e89dbbe0438f17c5400da9e03585
williamzshih <148172235+williamzshih@users.noreply.github.com>
2025-08-20 10:07:44 -0700

    configure middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/auth";
 
 export default auth(async (req) => {
@@ -21,6 +21,13 @@ export default auth(async (req) => {
   return NextResponse.next();
 });
 
+export const middleware = async (req: NextRequest) => {
+  const session = await auth();
+  if (!session?.user?.email)
+    return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
+  return NextResponse.next();
+};
+
 export const config = {
-  matcher: "/user/:path*",
+  matcher: "/",
 };
