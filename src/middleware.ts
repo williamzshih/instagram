65592e8cbdb3c30b8ab21950f29414175366a724
williamzshih <148172235+williamzshih@users.noreply.github.com>
2025-08-20 15:24:09 -0700

    remove middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/auth";
 
-export default auth(async (req) => {
-  const cookie = req.headers.get("cookie");
-  if (!cookie) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-  const response = await fetch(`${req.nextUrl.origin}/api/user`, {
-    headers: {
-      cookie: cookie,
-    },
-  });
-  const username = await response.json();
-  const pathSplit = req.nextUrl.pathname.split("/user/")[1].split("/");
-  const pathUsername = pathSplit[0];
-  const rest = pathSplit.slice(1).join("/");
-  if (username === pathUsername) {
-    return NextResponse.redirect(new URL(`/profile/${rest}`, req.nextUrl));
-  }
-  return NextResponse.next();
-});
-
 export const middleware = async (req: NextRequest) => {
   const session = await auth();
   if (!session?.user?.email)
